edd1b87a539803c8c5dda3ce7df4d3fe00a82435
vishnuparihar <vishnuparihar239925@gmail.com>
2025-01-21 00:56:49 +0530

    jts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
       <header className="bg-white shadow-md sticky top-0 text-gray-700 flex justify-between items-center ">
         <div id='logo' className=""><img className='h-20 px-4 ' src={jtslogo} alt="" /></div>
         <div id="navigations">
-          <ul className='flex items-center flex-wrap text-2xl gap-5 px-4'>
+          <ul className='flex items-center flex-wrap text-2xl font-semibold gap-5 px-4 sm:flex hidden'>
             <li><a href="">Home</a></li>
             <li><a href="">Services</a></li>
             <li><a href="">Coverage</a></li>

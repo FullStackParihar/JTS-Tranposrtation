f99415c0a34e1a3df3dd723e7dec74a02b52eb92
vishnuparihar <vishnuparihar239925@gmail.com>
2025-01-25 23:44:19 +0530

    jts

--- src/utility/FleetData.js
+++ src/utility/FleetData.js
@@ -9,7 +9,7 @@ const fleetData = [
     image: truckbg,
     capacity: "12,000 liters",
     description: "Reliable for long-distance bulk milk transportation with high efficiency.",
-    coordinates: [28.7041, 77.1025], // Example coordinates (Delhi)
+  
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ const fleetData = [
     image: pickuptruck,
     capacity: "5,000 liters",
     description: "Ideal for smaller loads and quick deliveries within the city.",
-    coordinates: [28.5355, 77.3910], // Example coordinates (Noida)
+    
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const fleetData = [
     image: accontainer,
     capacity: "12,000 liters",
     description: "Suitable for heavy-duty transport with robust performance similar to Eicher.",
-    coordinates: [19.0760, 72.8777], // Example coordinates (Mumbai)
+  
   },
 ];
 

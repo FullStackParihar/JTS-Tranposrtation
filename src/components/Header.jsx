198280eabbe184d6f599adf8e7a8c1f9e49e0a9f
vishnuparihar <vishnuparihar239925@gmail.com>
2025-02-05 23:18:11 +0530

    jts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -82,7 +82,7 @@ function Header() {
             <ul className="flex flex-col text-gray-800 font-semibold text-lg">
               <Link to="/"> <li className="p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]">Home</li></Link>
               <Link to="/services"><li className="p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]">Services</li></Link>
-              <li className="p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]"><Link to="/coverage">Coverage</Link></li>
+              <Link to="/coverage"><li className="p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]">Coverage</li></Link>
               <li className="p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]"><Link to="/fleet">Fleet</Link></li>
               <li className='p-3 bg-white text-[#6A1B9A] hover:bg-[#F57C00] hover:text-white animation border-[0.5px]'><Link to="/contact">Contact</Link></li>
             </ul>
